c5731507301cc5d96a302d89fad37647a5351f3e
AntonioAndraues <juniorbrgamers@gmail.com>
2020-06-19 12:11:18 -0300

    sorce Code

---
+++ Assets/Scripts/nave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nave : MonoBehaviour
+{
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tiro"))
+        {
+            SoundManager.PlaySound(SoundManager.Sound.Dead_nave);
+            Score.AddScore();
+            this.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
+}
